b488ae22d1f41d236dde956b9eeb9bd668442623
seokin <tzs930@gmail.com>
2017-02-11 13:33:45 +0900

    Add Docs Folder

--- Marco_beta_v1.0/AppDelegate.swift
+++ Marco_beta_v1.0/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         GMSServices.provideAPIKey("AIzaSyC_O5Gc3ThZJ_HjgHC-bf3SauVeOSfk0iI")
+        GMSPlacesClient.provideAPIKey("AIzaSyCVUZYCrUGGDtqEZ1ifFd43vf8lDcH_olQ")
         return true
     }
 
--- Marco_beta_v1.0/MapViewController.swift
+++ Marco_beta_v1.0/MapViewController.swift
@@ -8,16 +8,47 @@
 
 import UIKit
 import GoogleMaps
+import GooglePlaces
+import GooglePlacePicker
 
 class MapViewController: UIViewController {
+    var placeClient: GMSPlacesClient!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let camera = GMSCameraPosition.camera(withLatitude: 51.512253,longitude: -0.123205, zoom: 14)
+        // Load Current Location
+        placeClient = GMSPlacesClient.shared()
+        
+        // Draw MapView
+        
+        // London : 51.512253, -0.123205
+        
+        placeClient.currentPlace(callback: { (placeLikelihoodList, error) -> Void in
+            if let error = error {
+                print("Pick Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            print("No current place")
+            
+            if let placeLikelihoodList = placeLikelihoodList {
+                let place = placeLikelihoodList.likelihoods.first?.place
+                if let place = place {
+                    print( place.name )
+                    print( place.formattedAddress?.components(separatedBy: ", ").joined(separator: "\n") ?? "" )
+                    self.drawMap( placeLocation: place )
+                }
+            }
+        })
+        
+    }
+    
+    func drawMap( placeLocation: GMSPlace ) {
+        
+        let camera = GMSCameraPosition.camera(withLatitude: placeLocation.coordinate.latitude, longitude: placeLocation.coordinate.longitude, zoom: 14)
         let mapView = GMSMapView.map(withFrame: CGRect(x: 0, y: 0, width: 0, height: 0), camera: camera)
         mapView.isMyLocationEnabled = true
-        
         
         do {
             // Set the map style by passing the URL of the local file.
@@ -32,11 +63,39 @@ class MapViewController: UIViewController {
         
         self.view = mapView
         
+        // Draw Marker
+        /*
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(51.512253, -0.123205)
-        marker.title = "London"
-        marker.snippet = "United Kingdom"
+        marker.position = CLLocationCoordinate2DMake(placeLocation.coordinate.latitude, placeLocation.coordinate.longitude)
+        marker.appearAnimation = kGMSMarkerAnimationPop
+        marker.icon = UIImage(named: "flag_icon")
+        marker.title = placeLocation.name
+        marker.snippet = placeLocation.formattedAddress?.components(separatedBy: ", ").joined(separator: "\n") ?? ""
         marker.map = mapView
+         */
+    }
+    
+    
+    
+    @IBAction func refreshButtonClicked(_ sender: Any) {
+        
+        placeClient.currentPlace(callback: { (placeLikelihoodList, error) -> Void in
+            if let error = error {
+                print("Pick Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            print("No current place")
+            
+            if let placeLikelihoodList = placeLikelihoodList {
+                let place = placeLikelihoodList.likelihoods.first?.place
+                if let place = place {
+                    print( place.name )
+                    print( place.formattedAddress?.components(separatedBy: ", ").joined(separator: "\n") ?? "" )
+                    self.drawMap( placeLocation: place )
+                }
+            }
+        })
     }
     
 }
